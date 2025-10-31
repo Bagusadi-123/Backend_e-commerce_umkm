614b8992ee0aba1f940765e4f51d2497014fcc23
Bagusadi-123 <bagusadibrata4@gmail.com>
2025-10-31 13:35:50 +0700

    perubahan baru

--- router/user.js
+++ router/user.js
@@ -3,41 +3,76 @@ import User from '../model/user.js';
 
 const router = express.Router();
 
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) { //jika email pass kosong, nanti dicek juga di aplikasi
+        res.status(404).send({})
+    }
+    const result = await User.findOne({ $and:
+            [{ email: email }, { password: password }]
+    });
+    if (!result) {
+        res.status(404).send({'status': 'user not found'});
+    } else {
+        res.status(200).send(result);
+    }
+});
+
+
 //ambil semua data user (untuk admin)
 router.get('/', async(req, res) =>{
     const getUsers = await User.find();
     res.json(getUsers);
     });
 
-router.post('/', async (req, res) =>{
+router.post('/register', async (req, res) =>{
     try{
        //mengambil data dari body permintaan(postman / website)
-        const {nama, email, alamat} = req.body;
-
+        const {username, email, password, role, created_at, updated_at, deleted} = req.body;
+        console.log(req.body);
         //Buat objek data baru sesuai dengan Skema Mongoose
-        const newUser = {
-            nama,
-            email,
-            alamat
-        };
-        //menyimpan data baru ke mongodb
-        const userRegister = await User.create(newUser);
+        const userInsert = new User({
+            username: username,
+            email: email,
+            password: password,
+            role: role,
+            created_at: created_at,
+            updated_at: updated_at,
+            deleted: deleted,
+        });
+        const insert = await userInsert.save();
+        console.log(insert);
+        res.status(200).send(insert);
 
-        //mengirim respon ke klien
-        res.status(201).json({
-        message: "Pendaftaran pengguna berhasil", 
-        data: userRegister
-    });
-
-}catch (error) {
+    }catch (error) {
     //menangani jika ada kesalahan (misalnya email duplikat karena unique: true)
     console.error(error);
     res.status(500).json ({
         message: "Gagal memproses data",
         error: error.message
     });
-}
-    
+    }
 });
+    
+
+    // update data user
+    router.put('/:id', async (req, res) => { //TODO tambahkan ke tiap model/schema lain
+        const users = await User.findByIdAndUpdate(req.params.id, req.body,{}) //TODO ubah User menjadi tipe data sesuai model yang lain
+        res.status(200).send({
+            'status': true,
+            'data': users
+        });
+    });
+    
+    router.delete('/:id', async (req, res) => { //TODO tambahkan ke tiap model/schema lain
+        const users = await User.findByIdAndDelete(req.params.id, req.body) //TODO ubah User menjadi tipe data sesuai model yang lain
+        res.status(200).send({
+            'status': true,
+            'data': users
+        });
+    });
+
+
+    
 
 export default router;
