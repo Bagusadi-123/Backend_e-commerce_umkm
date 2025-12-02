6d2a70ec65cac89aa0d32ff33d56ff0b73e27327
Bagus <bagusadibrata4@gmail.com>
2025-12-03 06:30:47 +0700

    Add files via upload

--- Index.js
+++ Index.js
@@ -1,16 +1,17 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
-import express, {response} from 'express';
-import cartRoutes from './router/cart.js';
-import expeditionRoutes from './router/expedition.js';
-import orderRoutes from './router/order.js';
-import orderDetailRoutes from './router/orderDetail.js';
-import productRoutes from './router/product.js';
-import productFavorite from './router/productFavorite.js';
-import productReviewRoutes from './router/productReview.js';
-import storeDetailRoutes from './router/storeDetail.js';
+import express from 'express';
 import userRoutes from './router/user.js';
-import userDetailRoutes from './router/userDetail.js';
+import UserDetailRoutes from './router/userDetail.js';
+import CartRoutes from './router/cart.js';
+import ExpeditionRoutes from './router/expedition.js';
+import OrderRoutes from './router/order.js';
+import OrderDetailRoutes from './router/orderDetail.js';
+import ProductRoutes from './router/product.js';
+import ProductFavorite from "./model/productFavorite.js";
+import ProductReviewRoutes from './router/productReview.js';
+import StoreDetailRoutes from './router/storeDetail.js';
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -19,30 +20,42 @@ const dbName = process.env.DATABASE_NAME;
 
 //middleware untuk parsing atau mmegelola json 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 mongoose.connect(`${uri}/${dbName}`)
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 //ketik semua api endpoint disini
-app.use('/api/cart', cartRoutes);
-app.use('/api/expedition', expeditionRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/orderDetail', orderDetailRoutes);
-app.use('/api/product', productRoutes);
-app.use('/api/productFavorite', productFavorite)
-app.use('/api/productReview', productReviewRoutes);
-app.use('/api/storeDetail', storeDetailRoutes);
 app.use('/api/user', userRoutes);
-app.use('/api/userDetail', userDetailRoutes);
+// app.use('/api/user', UserDetailRoutes);
+app.use('/api/product/cart', CartRoutes);
+app.use('/api/expedition', ExpeditionRoutes);
+app.use('/api/order', OrderRoutes);
+// app.use('/api/order-detail', OrderDetailRoutes);
+app.use('/api/product', ProductRoutes);
+app.use('/api/product/favorite', ProductFavorite);
+app.use('/api/review', ProductReviewRoutes);
+app.use('/api/store', StoreDetailRoutes);
+
+//untuk lokasi foto, dan file lain mungkin
+app.use('/storage', express.static(path.resolve(process.cwd(), 'storage')));
 
 // testing api endpoint, bisa dicek di browser (atau aplikasi postman)
 // dan ketik localhost:4000/api/status
-app.get('/api/status', async (req, res)=> {
+app.get('/api/status', async (req, res) => {
     res.send({
         "status": "koneksi berhasil",
     });
 });
+
+//response jika method tidak tersedia/tidak bisa diakses
+//(misal di postman pakai POST ke url yang harusnya cuma GET
+//harus diletakkan di baris bawah sendiri, setelah semua route didefinisikan
+app.use((req, res) => {
+    res.status(404).json({error: "Not Found"});
+});
+
 //inisial REST API server
 app.listen(port, () => {
     console.log("Server listening on Port:", port);
