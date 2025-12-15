71c19bfbbcd73fd6d6967f1c9cd795c0c24efc4a
Bagus <bagusadibrata4@gmail.com>
2025-12-15 23:30:54 +0700

    Add stock field to product schema and update store ref

--- model/product.js
+++ model/product.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 
 const productSchema = new mongoose.Schema({
-    store: {type: mongoose.Schema.Types.ObjectId, ref: 'storeDetail'},
+    store: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
     name: {type: String, require: true},
     price: {type: Number, require: true},
     desc: {type: String, require: true},
     product_photo_path: {type: String, require: true},
+    stock: {type: Number, required: true},
     created_at: {type: Date, default: Date.now},
     updated_at: {type: Date, default: Date.now},
     deleted: {type: Boolean, default: false},
