0e5b045690c15f5e3e2ac00de58e66e88142fb40
Bagus <bagusadibrata4@gmail.com>
2025-12-15 23:28:58 +0700

    Enhance order schema with additional fields

--- model/order.js
+++ model/order.js
@@ -7,10 +7,15 @@ const orderSchema = new mongoose.Schema({
     qty: {type: Number, require: true},
     price: {type: Number, require: true},
     total: {type: String, require: true},
-    order_status: {type: String, default: orderStatus.BELUM_BAYAR},
+    order_status: {type: String, default: orderStatus.DITERIMA_EKSPEDISI},
     waybill: {type: String, require: true},
     payment_method: {type: String, required: true},
+    payment_status: {type: String, default: orderStatus.BELUM_BAYAR},
+    expedition_courier: {type: String, required: true},
     order_detail: [{type:mongoose.Schema.Types.ObjectId, ref: 'orderDetail'}],
+    created_at: {type: Date, default: Date.now},
+    updated_at: {type: Date, default: Date.now},
+    deleted: {type: Boolean, default: false}
 }, {versionKey: false});
 
 export default mongoose.model('order', orderSchema);
