e4624e43a62fa7689c9489030b3347b168e05bbb
Bagus <bagusadibrata4@gmail.com>
2025-12-15 23:27:08 +0700

    Remove commented code from cart schema

--- model/cart.js
+++ model/cart.js
@@ -4,9 +4,6 @@ const cartSchema = new mongoose.Schema({
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
     product: {type: mongoose.Schema.Types.ObjectId, ref: 'product'},
     qty: {type: Number, default: 1}, //quantity, jumlah
-    // created_at: {type: Date, default: Date.now},
-    // updated_at: {type: Date, default: Date.now},
-    // deleted: {type: Boolean, default: false},
 }, {versionKey: false});
 
 export default mongoose.model("cart", cartSchema);
